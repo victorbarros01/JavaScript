ac9a93a95ddc11cd1b07316127caa774483a2a13
Victor E B Barros <victoremanuel6778@gmail.com>
2024-11-26 19:06:20 -0300

    26/11

--- JavaScript/Aula1 - numbers, string, boolean, operador tenario, etc/index.js
+++ JavaScript/Aula1 - numbers, string, boolean, operador tenario, etc/index.js
@@ -4,15 +4,15 @@ console.log(2 % 2);
 console.log(Math.pow(3, 2)); // potência
 console.log(Math.sqrt(25)); // raiz quadrada
 
-//exercicio 1
+//exercicio 1 - String
 
 console.log("String " + '"string"' + `${"template literals " + (1 + 1)}`);
 
-//exercicio 2
+//exercicio 2 - Number
 
 console.log(2, 4.5, 5 + 65);
 
-//exercicio 3
+//exercicio 3 - boolean
 
 if (5 > 10) {
   console.log("5 é maior que 10");
@@ -24,7 +24,7 @@ if (5 > 10) {
   console.log("5 não é igual a 10");
 }
 
-//exercicio 4
+//exercicio 4 - Operador Lógico
 
 if (19 - 30 < 17 && 19 - 29 > 10) {
   console.log("Condição verdadeira 1");
@@ -36,6 +36,6 @@ if (!19 - 30 < 17) {
   console.log("Condição verdadeira 3");
 }
 
-//exercicio 5
+//exercicio 5 - NaN
 
 console.log(100 * "assds");
